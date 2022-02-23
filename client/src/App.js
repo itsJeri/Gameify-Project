578909c8a1f6cb5558cee369356243573abaec16
Jerry Tong <zihangtong@gmail.com>
2022-02-22 22:40:25 -0500

    React setup

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    fetch('/hello')
+      .then(r => r.json())
+      .then(data => setCount(data.count));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Page Count: {count}</h1>
+    </div>
+  );
+}
+
+export default App;
